56f53cf1fd3d6c87cfb38aad630d308af180434d
Marc Jakobi <marc.jakobi@tiko.energy>
2024-01-19 14:47:33 +0100

    style: run rustfmt

--- rocks-lib/src/manifest/pull_manifest.rs
+++ rocks-lib/src/manifest/pull_manifest.rs
@@ -3,13 +3,23 @@ use reqwest::Client;
 use std::{fs, path::PathBuf};
 
 // TODO(vhyrro): Perhaps cache the manifest somewhere on disk?
-pub async fn manifest_from_server(url: String, lua_version: Option<&String>, cache: Option<PathBuf>) -> Result<String> {
-    let manifest_filename = "manifest".to_string() + &lua_version.map(|s| format!("-{}", s)).unwrap_or_default();
+pub async fn manifest_from_server(
+    url: String,
+    lua_version: Option<&String>,
+    cache: Option<PathBuf>,
+) -> Result<String> {
+    let manifest_filename =
+        "manifest".to_string() + &lua_version.map(|s| format!("-{}", s)).unwrap_or_default();
     let url = url + "/" + &manifest_filename;
 
     // Stores a path to the manifest cache (this allows us to operate on a manifest without
     // needing to pull it from the luarocks servers each time).
-    let cache = cache.unwrap_or_else(|| directories::ProjectDirs::from("org", "neorocks", "rocks").unwrap().cache_dir().join(&manifest_filename));
+    let cache = cache.unwrap_or_else(|| {
+        directories::ProjectDirs::from("org", "neorocks", "rocks")
+            .unwrap()
+            .cache_dir()
+            .join(&manifest_filename)
+    });
 
     // Ensure all intermediate directories for the cache file are created (e.g. `~/.cache/rocks/manifest`)
     fs::create_dir_all(cache.parent().unwrap())?;
@@ -41,11 +51,7 @@ pub async fn manifest_from_server(url: String, lua_version: Option<&String>, cac
 
     // If our cache file does not exist then pull the whole manifest.
 
-    let new_manifest = 
-        reqwest::get(url)
-            .await?
-            .text()
-            .await?;
+    let new_manifest = reqwest::get(url).await?.text().await?;
 
     fs::write(&cache, &new_manifest)?;
 
