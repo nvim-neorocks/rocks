f96cf941885b669f098765e7ba1209e9bbc62b03
Marc Jakobi <marc.jakobi@tiko.energy>
2024-01-28 17:08:32 +0100

    feat(lib/rockspac): parse source.url

---
+++ rocks-lib/src/rocks/rock_source.rs
@@ -0,0 +1,124 @@
+use eyre::{eyre, Result};
+use regex::RegexSet;
+use reqwest::Url;
+use serde::{de, Deserialize, Deserializer};
+use std::{path::PathBuf, str::FromStr};
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct RockSource {
+    #[serde(deserialize_with = "source_url_from_str")]
+    url: SourceUrl,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SourceUrl {
+    /// For the CVS source control manager
+    Cvs(String),
+    /// For URLs in the local filesystem
+    File(PathBuf),
+    /// Web URLs
+    Url(Url),
+    /// For the Git source control manager
+    Git(String),
+    /// or the Mercurial source control manager
+    Mercurial(String),
+    /// or the SurroundSCM source control manager
+    Sscm(String),
+    /// or the Subversion source control manager
+    Svn(String),
+}
+
+impl FromStr for SourceUrl {
+    type Err = eyre::Error;
+
+    fn from_str(str: &str) -> Result<Self> {
+        let url_regex_set: RegexSet =
+            RegexSet::new(&[r"^https://", r"^http://", r"^ftp://"]).unwrap();
+
+        let git_source_regex_set: RegexSet = RegexSet::new(&[
+            r"^git://",
+            r"^git\+file://",
+            r"^git\+http://",
+            r"^git\+https://",
+            r"^git\+ssh://",
+        ])
+        .unwrap();
+
+        let mercurial_source_regex_set: RegexSet =
+            RegexSet::new(&[r"^hg://", r"^hg\+http://", r"^hg\+https://", r"^hg\+ssh://"]).unwrap();
+
+        match str {
+            s if s.starts_with("cvs://") => Ok(Self::Cvs(s.to_string())),
+            s if s.starts_with("file://") => Ok(Self::File(s.trim_start_matches("file://").into())),
+            s if url_regex_set.matches(s).matched_any() => Ok(Self::Url(s.parse()?)),
+            s if git_source_regex_set.matches(s).matched_any() => Ok(Self::Git(s.to_string())),
+            s if mercurial_source_regex_set.matches(s).matched_any() => {
+                Ok(Self::Mercurial(s.to_string()))
+            }
+            s if s.starts_with("sscm://") => Ok(Self::Sscm(s.to_string())),
+            s if s.starts_with("svn://") => Ok(Self::Svn(s.to_string())),
+            s => Err(eyre!("Unsupported source URL: {}", s)),
+        }
+    }
+}
+
+fn source_url_from_str<'de, D>(deserializer: D) -> Result<SourceUrl, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    SourceUrl::from_str(&s).map_err(de::Error::custom)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::path::Path;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn parse_rock_source_url() {
+        let url: SourceUrl = "cvs://foo".parse().unwrap();
+        assert_eq!(url, SourceUrl::Cvs("cvs://foo".into()));
+        let url: SourceUrl = "cvs://bar".parse().unwrap();
+        assert_eq!(url, SourceUrl::Cvs("cvs://bar".into()));
+        let url: SourceUrl = "file:///tmp/foo".parse().unwrap();
+        let file = Path::new("/tmp/foo");
+        assert_eq!(url, SourceUrl::File(file.to_path_buf()));
+        let url: SourceUrl = "ftp://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Url { .. }));
+        let url: SourceUrl = "git://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Git { .. }));
+        let url: SourceUrl = "git+file://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Git { .. }));
+        let url: SourceUrl = "git+http://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Git { .. }));
+        let url: SourceUrl = "git+https://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Git { .. }));
+        let url: SourceUrl = "git+ssh://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Git { .. }));
+        let _err = SourceUrl::from_str("git+foo://example.com/foo").unwrap_err();
+        let url: SourceUrl = "hg://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Mercurial { .. }));
+        let url: SourceUrl = "hg+http://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Mercurial { .. }));
+        let url: SourceUrl = "hg+https://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Mercurial { .. }));
+        let url: SourceUrl = "hg+ssh://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Mercurial { .. }));
+        let _err = SourceUrl::from_str("hg+foo://example.com/foo").unwrap_err();
+        let url: SourceUrl = "https://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Url { .. }));
+        let url: SourceUrl = "http://example.com/foo".parse().unwrap();
+        assert!(matches!(url, SourceUrl::Url { .. }));
+        let url: SourceUrl = "sscm://foo".parse().unwrap();
+        assert_eq!(url, SourceUrl::Sscm("sscm://foo".into()));
+        let url: SourceUrl = "sscm://bar".parse().unwrap();
+        assert_eq!(url, SourceUrl::Sscm("sscm://bar".into()));
+        let url: SourceUrl = "svn://foo".parse().unwrap();
+        assert_eq!(url, SourceUrl::Svn("svn://foo".into()));
+        let url: SourceUrl = "svn://bar".parse().unwrap();
+        assert_eq!(url, SourceUrl::Svn("svn://bar".into()));
+    }
+}
