dbb0804b0af5ce8e875a0445d2b8c90929427802
Marc Jakobi <mrcjkb89@outlook.com>
2024-01-31 01:14:25 +0100

    refactor(rockspec): move rocks.rockspec to its own module (#7)

--- rocks-lib/src/rocks/mod.rs
+++ rocks-lib/src/rocks/mod.rs
@@ -1,13 +1,5 @@
-mod dependency;
 mod download;
-mod platform;
-mod rock_source;
-mod rockspec;
 mod unpack;
 
-pub use dependency::*;
 pub use download::*;
-pub use platform::*;
-pub use rock_source::*;
-pub use rockspec::*;
 pub use unpack::*;
