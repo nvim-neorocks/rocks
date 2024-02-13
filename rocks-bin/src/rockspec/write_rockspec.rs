fd1d077deed3028540b8ffaccd5ee4b1c4c259eb
Vhyrro <vhyrro@gmail.com>
2024-02-13 16:27:23 +0100

    feat(bin/write-rockspec): add `directory` argument, don't query github metadata if all data has already been supplied

--- rocks-bin/src/rockspec/write_rockspec.rs
+++ rocks-bin/src/rockspec/write_rockspec.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{str::FromStr, path::PathBuf};
 
 use clap::Args;
 use eyre::{eyre, Result};
@@ -41,12 +41,15 @@ macro_rules! parse {
 
 // General notes and ideas:
 // - Should we require the user to create a "project" in order to use this command?
-// - Ask user for a homepage
 // - Automatically detect build type by inspecting the current repo (is there a Cargo.toml? is
 //   there something that tells us it's a lua project?).
+// - If repo metadata couldn't be fetched then don't error, simply don't provide default values.
 
 #[derive(Args)]
 pub struct WriteRockspec {
+    /// The directory of the rock.
+    directory: Option<PathBuf>,
+
     /// The name to give to the rock.
     #[arg(long)]
     name: Option<String>,
@@ -118,132 +121,151 @@ fn parse_version(input: String) -> Result<LuaDependency> {
     LuaDependency::from_str(format!("lua {}", input).as_str())
 }
 
-pub async fn write_rockspec(data: WriteRockspec) -> Result<()> {
-    let mut spinner = Spinner::new(Spinners::Dots, "Fetching repository metadata...".into());
+pub async fn write_rockspec(cli_flags: WriteRockspec) -> Result<()> {
+    let (package_name, description, labels, license, lua_versions, maintainer) = match (
+        cli_flags.name,
+        cli_flags.description,
+        cli_flags.labels,
+        cli_flags.license,
+        cli_flags.lua_versions,
+        cli_flags.maintainer,
+    ) {
+        ( // If all parameters are provided then don't bother prompting the user
+            Some(name),
+            Some(description),
+            Some(labels),
+            Some(license),
+            Some(lua_versions),
+            Some(maintainer),
+        ) => Ok::<_, eyre::Report>((name, description, labels, license, lua_versions, maintainer)),
+        (name, description, labels, license, lua_versions, maintainer) => {
+            let mut spinner =
+                Spinner::new(Spinners::Dots, "Fetching repository metadata...".into());
 
-    // [data.labels, data.license].into_iter().all(Option::is_some);
+            let repo_metadata = github_metadata::get_metadata_for(cli_flags.directory.as_ref())
+                .await?
+                .unwrap_or_else(|| RepoMetadata {
+                    name: std::env::current_dir()
+                        .expect("unable to get current working directory")
+                        .file_name()
+                        .unwrap_or_default()
+                        .to_string_lossy()
+                        .to_string(),
+                    description: None,
+                    license: None,
+                    contributors: vec![users::get_current_username()
+                        .unwrap_or_default()
+                        .to_string_lossy()
+                        .to_string()],
+                    labels: None,
+                });
 
-    let repo_metadata = github_metadata::get_metadata_for(None)
-        .await?
-        .unwrap_or_else(|| RepoMetadata {
-            name: std::env::current_dir()
-                .expect("unable to get current working directory")
-                .file_name()
-                .unwrap_or_default()
-                .to_string_lossy()
-                .to_string(),
-            description: None,
-            license: None,
-            contributors: vec![users::get_current_username()
-                .unwrap_or_default()
-                .to_string_lossy()
-                .to_string()],
-            labels: None,
-        });
+            spinner.stop();
 
-    spinner.stop();
+            let mut editor = rustyline::Editor::<(), _>::new()?;
 
-    let mut editor = rustyline::Editor::<(), _>::new()?;
+            // TODO: Make prompts coloured
 
-    // TODO: Make prompts coloured
+            // let mut stdout = BufferedStandardStream::stdout(ColorChoice::Always);
+            // stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
 
-    // let mut stdout = BufferedStandardStream::stdout(ColorChoice::Always);
-    // stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
-
-    let package_name = data.name.map(Ok).unwrap_or_else(|| {
-        parse!(
-            editor
-                .readline(format!("Package Name (empty for '{}'): ", repo_metadata.name).as_str()),
-            identity,
-            repo_metadata.name
-        )
-    })?;
-
-    let description = data.description.map(Ok).unwrap_or_else(|| {
-        parse!(
-            editor.readline(
-                format!(
-                    "Description (empty for '{}'): ",
-                    repo_metadata
-                        .description
-                        .as_ref()
-                        .unwrap_or(&"*** enter a description ***".to_string())
+            let package_name = name.map(Ok).unwrap_or_else(|| {
+                parse!(
+                    editor.readline(
+                        format!("Package Name (empty for '{}'): ", repo_metadata.name).as_str()
+                    ),
+                    identity,
+                    repo_metadata.name
                 )
-                .as_str()
-            ),
-            identity,
-            repo_metadata.description.unwrap_or_default()
-        )
-    })?;
+            })?;
 
-    let license = data
-        .license // First validate the `--license` option
-        .map(Ok)
-        .unwrap_or_else(|| {
-            // If there was no `--license` then prompt the user
-            parse!(
-                editor.readline(
-                    format!(
-                        "License (empty for '{}', 'none' for no license): ",
-                        repo_metadata
-                            .license
-                            .as_ref()
-                            .unwrap_or(&"none".to_string())
+            let description = description.map(Ok).unwrap_or_else(|| {
+                parse!(
+                    editor.readline(
+                        format!(
+                            "Description (empty for '{}'): ",
+                            repo_metadata
+                                .description
+                                .as_ref()
+                                .unwrap_or(&"*** enter a description ***".to_string())
+                        )
+                        .as_str()
+                    ),
+                    identity,
+                    repo_metadata.description.unwrap_or_default()
+                )
+            })?;
+
+            let license = license // First validate the `--license` option
+                .map(Ok)
+                .unwrap_or_else(|| {
+                    // If there was no `--license` then prompt the user
+                    parse!(
+                        editor.readline(
+                            format!(
+                                "License (empty for '{}', 'none' for no license): ",
+                                repo_metadata
+                                    .license
+                                    .as_ref()
+                                    .unwrap_or(&"none".to_string())
+                            )
+                            .as_str()
+                        ),
+                        parse_license,
+                        parse_license(repo_metadata.license.unwrap())?
                     )
-                    .as_str()
-                ),
-                parse_license,
-                parse_license(repo_metadata.license.unwrap())?
-            )
-        })?
-        .map(|license| license.name)
-        .unwrap_or("*** enter a license ***");
+                })?;
 
-    let maintainer = data.maintainer.map(Ok).unwrap_or_else(|| {
-        parse!(
-            editor.readline(
-                format!(
-                    "Maintainer (empty for '{}'): ",
+            let maintainer = maintainer.map(Ok).unwrap_or_else(|| {
+                parse!(
+                    editor.readline(
+                        format!(
+                            "Maintainer (empty for '{}'): ",
+                            repo_metadata.contributors.first().unwrap_or(&"".into())
+                        )
+                        .as_str()
+                    ),
+                    identity,
                     repo_metadata.contributors.first().unwrap_or(&"".into())
                 )
-                .as_str()
-            ),
-            identity,
-            repo_metadata.contributors.first().unwrap_or(&"".into())
-        )
-    })?;
+            })?;
 
-    let labels = data
-        .labels
-        .map(Ok)
-        .unwrap_or_else(|| {
-            parse!(
-                editor.readline(
-                    format!(
-                        "Labels (empty for '[{}]'): ",
-                        repo_metadata
-                            .labels
-                            .as_ref()
-                            .unwrap_or(&Vec::default())
-                            .join(" ")
-                    )
-                    .as_str()
-                ),
-                parse_list,
-                repo_metadata.labels.unwrap_or_default()
-            )
-        })?
-        .into_iter()
-        .map(|label| "\"".to_string() + &label + "\"")
-        .join(", ");
+            let labels = labels.map(Ok).unwrap_or_else(|| {
+                parse!(
+                    editor.readline(
+                        format!(
+                            "Labels (empty for '[{}]'): ",
+                            repo_metadata
+                                .labels
+                                .as_ref()
+                                .unwrap_or(&Vec::default())
+                                .join(" ")
+                        )
+                        .as_str()
+                    ),
+                    parse_list,
+                    repo_metadata.labels.unwrap_or_default()
+                )
+            })?;
 
-    let lua_versions = data.lua_versions.map(Ok).unwrap_or_else(|| {
-        parse!(
-            editor.readline("Supported Lua Versions (empty for '>= 5.1'): ",),
-            parse_version,
-            LuaDependency::from_str("lua >= 5.1")?
-        )
-    })?;
+            let lua_versions = lua_versions.map(Ok).unwrap_or_else(|| {
+                parse!(
+                    editor.readline("Supported Lua Versions (empty for '>= 5.1'): ",),
+                    parse_version,
+                    LuaDependency::from_str("lua >= 5.1")?
+                )
+            })?;
+
+            Ok((
+                package_name,
+                description,
+                labels,
+                license,
+                lua_versions,
+                maintainer,
+            ))
+        }
+    }?;
 
     std::fs::write(
         format!("{}-dev.rockspec", package_name),
@@ -274,8 +296,13 @@ build = {{
     "#,
             package_name = package_name,
             summary = description,
-            license = license,
-            labels = labels,
+            license = license
+                .map(|license| license.name)
+                .unwrap_or("*** enter a license ***"),
+            labels = labels
+                .into_iter()
+                .map(|label| "\"".to_string() + &label + "\"")
+                .join(", "),
             maintainer = maintainer,
             version = lua_versions.rock_version_req.to_string().replace('^', "~>"),
         )
