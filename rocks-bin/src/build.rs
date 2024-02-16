75f38ae8d9bee2a6136b5966e927f4a4f4f0a657
Vhyrro <vhyrro@gmail.com>
2024-02-16 18:04:44 +0100

    feat(bin): add 'build' command skeleton

---
+++ rocks-bin/src/build.rs
@@ -0,0 +1,24 @@
+use std::path::PathBuf;
+use eyre::eyre;
+
+use clap::Args;
+use eyre::Result;
+use rocks_lib::rockspec::Rockspec;
+
+#[derive(Args)]
+pub struct Build {
+    directory: PathBuf,
+}
+
+pub fn build(data: Build) -> Result<()> {
+    if data.directory.extension().map(|ext| ext != "rockspec").unwrap_or(true) {
+        return Err(eyre!("Provided path is not a valid rockspec!"));
+    }
+
+    let rockspec = String::from_utf8(std::fs::read(data.directory)?)?;
+    let rockspec = Rockspec::new(&rockspec)?;
+
+    rocks_lib::build::build(rockspec, false)?;
+
+    Ok(())
+}
