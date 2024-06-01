3a42fec5bde71572b45606420a251439b76c3757
Vhyrro <vhyrro@gmail.com>
2024-06-01 13:03:47 +0200

    feat(lib): properly install rockspec sources, properly enumerate and autodetect Lua modules

--- rocks-lib/src/build/mod.rs
+++ rocks-lib/src/build/mod.rs
@@ -1,13 +1,40 @@
-
-use crate::rockspec::{Rockspec, Build, BuildBackendSpec};
+use crate::rockspec::{Build, BuildBackendSpec, RockSourceSpec, Rockspec};
 use eyre::Result;
+use git2::Repository;
 
 pub fn build(rockspec: Rockspec, no_install: bool) -> Result<()> {
+    // TODO(vhyrro): Use a more serious isolation strategy here.
+    let temp_dir = tempdir::TempDir::new(&rockspec.package)?;
+
+    let previous_dir = std::env::current_dir()?;
+
+    // Install the source in order to build.
+    match &rockspec.source.current_platform().source_spec {
+        RockSourceSpec::Git(git) => {
+            let repo = Repository::clone(&git.url.to_string(), &temp_dir)?;
+
+            if let Some(commit_hash) = &git.checkout_ref {
+                let (object, _) = repo.revparse_ext(commit_hash)?;
+                repo.checkout_tree(&object, None)?;
+            }
+        }
+        RockSourceSpec::Url(_) => todo!(),
+        RockSourceSpec::File(_) => todo!(),
+        RockSourceSpec::Cvs(_) => unimplemented!(),
+        RockSourceSpec::Mercurial(_) => unimplemented!(),
+        RockSourceSpec::Sscm(_) => unimplemented!(),
+        RockSourceSpec::Svn(_) => unimplemented!(),
+    };
+
+    std::env::set_current_dir(&temp_dir)?;
+
     // TODO: Ensure dependencies and build dependencies.
     match rockspec.build.default.build_backend.as_ref().cloned() {
         Some(BuildBackendSpec::Builtin(spec)) => spec.run(rockspec, no_install)?,
         _ => unimplemented!(),
     };
+
+    std::env::set_current_dir(previous_dir)?;
 
     Ok(())
 }
