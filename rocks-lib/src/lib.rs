4f6dd85d9a9df577adf0f9300037b3d4d422ce01
Vhyrro <vhyrro@gmail.com>
2024-06-01 21:46:18 +0200

    feat: add `Tree` data type

--- rocks-lib/src/lib.rs
+++ rocks-lib/src/lib.rs
@@ -3,3 +3,4 @@ pub mod config;
 pub mod manifest;
 pub mod rocks;
 pub mod rockspec;
+pub mod tree;
---
+++ rocks-lib/src/tree/mod.rs
@@ -0,0 +1,58 @@
+use std::path::PathBuf;
+
+use crate::config::LuaVersion;
+use eyre::Result;
+
+/// A tree is a collection of files where installed rocks are located.
+///
+/// `rocks` diverges from the traditional hierarchy employed by luarocks.
+/// Instead, we opt for a much simpler approach:
+///
+/// - /rocks/<lua-version> - contains rocks
+/// - /rocks/<lua-version>/<rock>/etc - documentation and supplementary files for the rock
+/// - /rocks/<lua-version>/<rock>/lib - shared libraries (.so files)
+/// - /rocks/<lua-version>/<rock>/src - library code for the rock
+/// - /bin - binary files produced by various rocks
+
+pub struct Tree<'a> {
+    /// The Lua version of the tree.
+    version: &'a LuaVersion,
+    /// The root of the tree.
+    root: &'a PathBuf,
+}
+
+/// Change-agnostic way of referencing various paths for a rock.
+pub struct TreeLayout {
+    pub etc: PathBuf,
+    pub lib: PathBuf,
+    pub src: PathBuf,
+}
+
+impl<'a> Tree<'a> {
+    pub fn new(root: &'a PathBuf, version: &'a LuaVersion) -> Result<Self> {
+        // Ensure that the root and the version directory exists.
+        std::fs::create_dir_all(root.join(version.to_string()))?;
+
+        Ok(Self { root, version })
+    }
+
+    pub fn root(&self) -> PathBuf {
+        self.root.join(self.version.to_string())
+    }
+
+    pub fn rock(&self, rock_name: &String, rock_version: &String) -> Result<TreeLayout> {
+        // TODO(vhyrro): Don't store rocks with the revision number, that should be stripped almost
+        // everywhere by default.
+        let rock_path = self.root().join(format!("{}@{}", rock_name, rock_version));
+
+        let etc = rock_path.join("etc");
+        let lib = rock_path.join("lib");
+        let src = rock_path.join("src");
+
+        std::fs::create_dir_all(&etc)?;
+        std::fs::create_dir_all(&lib)?;
+        std::fs::create_dir_all(&src)?;
+
+        Ok(TreeLayout { etc, lib, src })
+    }
+}
