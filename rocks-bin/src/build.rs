bdec672f866f261df4775ddcedf9461d096e6aa6
Vhyrro <vhyrro@gmail.com>
2024-09-10 18:20:07 +0200

    feat(`rocks build`): infer the rockspec location in a given directory

--- rocks-bin/src/build.rs
+++ rocks-bin/src/build.rs
@@ -1,4 +1,5 @@
 use eyre::eyre;
+use itertools::Itertools;
 use std::path::PathBuf;
 
 use clap::Args;
@@ -7,12 +8,36 @@ use rocks_lib::{config::Config, rockspec::Rockspec};
 
 #[derive(Args)]
 pub struct Build {
-    rockspec_path: PathBuf,
+    rockspec_path: Option<PathBuf>,
 }
 
 pub fn build(data: Build, config: &Config) -> Result<()> {
-    if data
-        .rockspec_path
+    let rockspec_path = data.rockspec_path.map_or_else(|| {
+        // Try to infer the rockspec the user meant.
+
+        let cwd = std::env::current_dir()?;
+
+        let rockspec_paths = walkdir::WalkDir::new(cwd)
+            .max_depth(1)
+            .same_file_system(true)
+            .into_iter()
+            .filter_map(|entry| entry.ok())
+            .filter(|entry| {
+                entry.file_type().is_file()
+                    && entry.path().extension().map(|ext| ext.to_str()) == Some(Some("rockspec"))
+            })
+            .collect_vec();
+
+        let rockspec_count = rockspec_paths.len();
+
+        match rockspec_count {
+            0 => Err(eyre!("No rockspec files found in the current directory!")),
+            1 => Ok(rockspec_paths.first().unwrap().clone().into_path()),
+            _ => Err(eyre!("Could not infer the rockspec to use! There are {} rockspecs in the current directory, please provide a path to the one you'd like to use.", rockspec_count)),
+        }
+    }, Ok)?;
+
+    if rockspec_path
         .extension()
         .map(|ext| ext != "rockspec")
         .unwrap_or(true)
@@ -20,7 +45,7 @@ pub fn build(data: Build, config: &Config) -> Result<()> {
         return Err(eyre!("Provided path is not a valid rockspec!"));
     }
 
-    let rockspec = String::from_utf8(std::fs::read(data.rockspec_path)?)?;
+    let rockspec = std::fs::read_to_string(rockspec_path)?;
     let rockspec = Rockspec::new(&rockspec)?;
 
     rocks_lib::build::build(rockspec, config)?;
