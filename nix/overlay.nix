1fca618f8fc643ca737f47ae96e614afa1e8201b
Marc Jakobi <marc.jakobi@tiko.energy>
2024-09-12 13:22:05 +0200

    ci: don't run release build on PR + run on mac

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,30 +1,49 @@
-{self}: final: prev: {
-  rocks = with final;
-    rustPlatform.buildRustPackage {
-      pname = "rocks";
-      version = ((lib.importTOML "${self}/rocks-bin/Cargo.toml").package).version;
+{self}: final: prev: let
+  # can't seem to override the buildType with override or overrideAttrs :(
+  mk-rocks = {buildType ? "release"}:
+    with final;
+      rustPlatform.buildRustPackage {
+        pname = "rocks";
+        version = ((lib.importTOML "${self}/rocks-bin/Cargo.toml").package).version;
 
-      src = self;
+        src = self;
 
-      cargoLock = {
-        lockFile = ../Cargo.lock;
+        cargoLock = {
+          lockFile = ../Cargo.lock;
+        };
+
+        nativeBuildInputs = [
+          pkg-config
+        ];
+
+        buildInputs =
+          [
+            luajit
+            openssl
+            libgit2
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+        nativeCheckInputs = [
+          cacert
+        ];
+
+        preCheck = ''
+          export HOME=$(realpath .)
+        '';
+
+        env = {
+          # disable vendored packages
+          LIBGIT2_NO_VENDOR = 1;
+          LIBSSH2_SYS_USE_PKG_CONFIG = 1;
+        };
+
+        inherit buildType;
       };
-
-      nativeBuildInputs = [
-        pkg-config
-      ];
-
-      buildInputs = [
-        luajit
-        openssl
-      ];
-
-      nativeCheckInputs = [
-        cacert
-      ];
-
-      preCheck = ''
-        export HOME=$(realpath .)
-      '';
-    };
+in {
+  rocks = mk-rocks {};
+  rocks-debug = mk-rocks {buildType = "debug";};
 }
