c72cc91e9a826a72af8c0e5675cccafe2182b35e
Vhyrro <vhyrro@gmail.com>
2024-09-12 21:18:42 +0200

    refactor(`install-lua`): remove positional argument, make use of `--lua-version`

--- rocks-bin/src/install_lua.rs
+++ rocks-bin/src/install_lua.rs
@@ -1,21 +1,16 @@
-use clap::Args;
-use eyre::Result;
-use rocks_lib::{
-    config::{Config, LuaVersion},
-    lua_installation::LuaInstallation,
-};
+use eyre::{OptionExt, Result};
+use rocks_lib::{config::Config, lua_installation::LuaInstallation};
 
-#[derive(Args)]
-pub struct InstallLua {
-    version: LuaVersion,
-}
-
-pub fn install_lua(install_data: InstallLua, config: &Config) -> Result<()> {
-    let version_stringified = install_data.version.to_string();
+pub fn install_lua(config: &Config) -> Result<()> {
+    // TODO: Make a single, monolithic getter for the lua version that returns a Result<>
+    let version_stringified = config
+        .lua_version
+        .as_ref()
+        .ok_or_eyre("lua version not set! Please provide it via `--lua-version`.")?;
 
     // TODO: Detect when path already exists by checking `Lua::path()` and prompt the user
     // whether they'd like to forcefully reinstall.
-    LuaInstallation::new(&install_data.version, config)?;
+    LuaInstallation::new(version_stringified, config)?;
 
     print!("Succesfully installed Lua {version_stringified}.");
 
