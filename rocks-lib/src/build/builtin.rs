4edb5296c4dcf841beb47fefa39513a249dcafb9
Vhyrro <vhyrro@gmail.com>
2024-09-14 13:17:04 +0200

    fix: broken tests

--- rocks-lib/src/build/builtin.rs
+++ rocks-lib/src/build/builtin.rs
@@ -104,8 +104,8 @@ mod tests {
         let rockspec = Rockspec::new(&content).unwrap();
 
         let config = Config::new()
-            .tree(Some(dir.into_path()))
-            .lua_version(Some(crate::config::LuaVersion::Lua51));
+            .tree(dir.into_path())
+            .lua_version(crate::config::LuaVersion::Lua51);
 
         crate::build::build(rockspec, &config).unwrap();
     }
