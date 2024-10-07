8618639981b3434a5a1f0968045e92df35690e5c
Marc Jakobi <marc.jakobi@tiko.energy>
2024-10-07 20:46:11 +0200

    feat!(unpack): don't download src.rock to disk

--- rocks-bin/src/download.rs
+++ rocks-bin/src/download.rs
@@ -12,9 +12,13 @@ pub struct Download {
 pub async fn download(dl_data: Download, config: Config) -> Result<()> {
     println!("Downloading {}...", dl_data.package_req.name());
 
-    let rock =
-        rocks_lib::operations::download(&MultiProgress::new(), &dl_data.package_req, None, &config)
-            .await?;
+    let rock = rocks_lib::operations::download_to_file(
+        &MultiProgress::new(),
+        &dl_data.package_req,
+        None,
+        &config,
+    )
+    .await?;
 
     println!("Succesfully downloaded {}@{}", rock.name, rock.version);
 
