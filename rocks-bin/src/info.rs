51e3cb9d550508e7e07cfb5812abd97a4ed649d9
vhyrro <vhyrro@gmail.com>
2024-10-11 14:10:40 +0200

    feat: add `rocks info` command

---
+++ rocks-bin/src/info.rs
@@ -0,0 +1,33 @@
+use clap::Args;
+use eyre::Result;
+use indicatif::MultiProgress;
+use rocks_lib::{
+    config::{Config, LuaVersion}, operations::download_rockspec, package::PackageReq, tree::Tree
+};
+
+#[derive(Args)]
+pub struct Info {
+    package: PackageReq,
+}
+
+pub async fn info(data: Info, config: Config) -> Result<()> {
+    // TODO(vhyrro): Add `Tree::from(&Config)`
+    let tree = Tree::new(config.tree().clone(), LuaVersion::from(&config)?)?;
+
+    let rockspec = download_rockspec(&MultiProgress::new(), &data.package, &config).await?;
+
+    if tree.has_rock(&data.package).is_some() {
+        println!("Currently installed in {}", tree.root().display());
+    }
+
+    println!("Package name: {}", rockspec.package);
+    println!("Package version: {}", rockspec.version);
+    println!();
+
+    println!("Summary: {}", rockspec.description.summary.unwrap_or("None".into()));
+    println!("Description: {}", rockspec.description.detailed.unwrap_or("None".into()));
+    println!("License: {}", rockspec.description.license.unwrap_or("None (all rights reserved by the author)".into()));
+    println!("Maintainer: {}", rockspec.description.maintainer.unwrap_or("Unspecified".into()));
+
+    Ok(())
+}
