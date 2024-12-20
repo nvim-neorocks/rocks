4154cbb4489992bf7d41bec25986d0897acd3edb
Marc Jakobi <marc.jakobi@tiko.energy>
2024-12-20 20:01:49 +0100

    fix: duplicated progress bars

--- rocks-lib/tests/test.rs
+++ rocks-lib/tests/test.rs
@@ -4,7 +4,7 @@ use rocks_lib::{
     config::ConfigBuilder,
     manifest::ManifestMetadata,
     operations::{ensure_busted, run_tests, TestEnv},
-    progress::{MultiProgress, Progress},
+    progress::MultiProgress,
     project::Project,
     tree::Tree,
 };
@@ -19,14 +19,9 @@ async fn run_busted_test() {
     let config = ConfigBuilder::new().tree(Some(tree_root)).build().unwrap();
     let tree = Tree::new(config.tree().clone(), config.lua_version().unwrap().clone()).unwrap();
     let manifest = ManifestMetadata::from_config(&config).await.unwrap();
-    ensure_busted(
-        &tree,
-        &manifest,
-        &config,
-        &Progress::Progress(MultiProgress::new()),
-    )
-    .await
-    .unwrap();
+    ensure_busted(&tree, &manifest, &config, MultiProgress::new_arc())
+        .await
+        .unwrap();
     run_tests(project, Vec::new(), TestEnv::Pure, config)
         .await
         .unwrap()
