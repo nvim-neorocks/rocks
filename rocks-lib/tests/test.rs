aa54bace1cd569677b8a59ed058a2a9af53cbdb7
Marc Jakobi <marc.jakobi@tiko.energy>
2025-01-02 17:02:46 +0100

    refactor!(test): use builder pattern

--- rocks-lib/tests/test.rs
+++ rocks-lib/tests/test.rs
@@ -1,12 +1,6 @@
 use std::path::PathBuf;
 
-use rocks_lib::{
-    config::ConfigBuilder,
-    operations::{ensure_busted, run_tests, TestEnv},
-    progress::MultiProgress,
-    project::Project,
-    tree::Tree,
-};
+use rocks_lib::{config::ConfigBuilder, operations::Test, project::Project};
 
 #[tokio::test]
 async fn run_busted_test() {
@@ -16,11 +10,5 @@ async fn run_busted_test() {
     let tree_root = project.root().to_path_buf().join(".rocks");
     let _ = std::fs::remove_dir_all(&tree_root);
     let config = ConfigBuilder::new().tree(Some(tree_root)).build().unwrap();
-    let tree = Tree::new(config.tree().clone(), config.lua_version().unwrap().clone()).unwrap();
-    ensure_busted(&tree, &config, MultiProgress::new_arc())
-        .await
-        .unwrap();
-    run_tests(project, Vec::new(), TestEnv::Pure, config)
-        .await
-        .unwrap()
+    Test::new(project, &config).run().await.unwrap();
 }
