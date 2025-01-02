c9d58cf0b4ccc0e165c35dd1b689d675358155ce
Marc Jakobi <marc.jakobi@tiko.energy>
2025-01-02 17:58:05 +0100

    feat!(update/remove): parallelize + use builder pattern

--- rocks-bin/src/pin.rs
+++ rocks-bin/src/pin.rs
@@ -4,6 +4,7 @@ use eyre::Result;
 use rocks_lib::lockfile::PinnedState;
 use rocks_lib::operations;
 use rocks_lib::package::PackageSpec;
+use rocks_lib::tree::RockMatches;
 use rocks_lib::{
     config::{Config, LuaVersion},
     tree::Tree,
@@ -17,11 +18,13 @@ pub struct ChangePin {
 pub fn set_pinned_state(data: ChangePin, config: Config, pin: PinnedState) -> Result<()> {
     let tree = Tree::new(config.tree().clone(), LuaVersion::from(&config)?)?;
 
-    if let Some(mut rock) = tree.has_rock_and(&data.package.clone().into_package_req(), |package| {
+    match tree.match_rocks_and(&data.package.clone().into_package_req(), |package| {
         pin != package.pinned()
-    }) {
-        Ok(operations::set_pinned_state(&mut rock, &tree, pin)?)
-    } else {
-        Err(eyre!("Rock {} not found!", data.package))
+    })? {
+        RockMatches::Single(mut rock) => Ok(operations::set_pinned_state(&mut rock, &tree, pin)?),
+        RockMatches::Many(_) => {
+            todo!("Add an error here about many conflicting types and to use `all:`")
+        }
+        RockMatches::NotFound(_) => Err(eyre!("Rock {} not found!", data.package)),
     }
 }
