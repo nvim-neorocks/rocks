9e30cf7fd67cca13c2a6f297f517874cbdd5d90a
Marc Jakobi <marc@jakobi.dev>
2025-02-18 21:04:12 +0100

    feat(lockfile): don't serialise dependencies fields if empty (#430)

--- lux-lib/src/lockfile/mod.rs
+++ lux-lib/src/lockfile/mod.rs
@@ -505,6 +505,10 @@ impl LocalPackageLock {
         self.rocks.get(id)
     }
 
+    fn is_empty(&self) -> bool {
+        self.entrypoints.is_empty()
+    }
+
     pub(crate) fn rocks(&self) -> &BTreeMap<LocalPackageId, LocalPackage> {
         &self.rocks
     }
@@ -640,11 +644,11 @@ pub struct ProjectLockfile<P: LockfilePermissions> {
     #[serde(skip)]
     _marker: PhantomData<P>,
     version: String,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "LocalPackageLock::is_empty")]
     dependencies: LocalPackageLock,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "LocalPackageLock::is_empty")]
     test_dependencies: LocalPackageLock,
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "LocalPackageLock::is_empty")]
     build_dependencies: LocalPackageLock,
 }
 
