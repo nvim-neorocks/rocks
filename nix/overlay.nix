c9f08eecc49ce16b8f1b5d75865edee27af3d5bf
Marc Jakobi <marc@jakobi.dev>
2025-02-26 21:41:28 +0100

    build: use crane (#442)

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,58 +1,66 @@
-{self}: final: prev: let
+{
+  self,
+  crane,
+}: final: prev: let
+  cleanCargoSrc = craneLib.cleanCargoSource self;
+
+  craneLib = crane.mkLib prev;
+
+  commonArgs = with final; {
+    inherit (craneLib.crateNameFromCargoToml {cargoToml = "${self}/lux-cli/Cargo.toml";}) version pname;
+    strictDeps = true;
+
+    nativeBuildInputs = [
+      pkg-config
+      installShellFiles
+    ];
+
+    buildInputs =
+      [
+        luajit
+        openssl
+        libgit2
+        gnupg
+        libgpg-error
+        gpgme
+      ]
+      ++ lib.optionals stdenv.isDarwin [
+        darwin.apple_sdk.frameworks.Security
+        darwin.apple_sdk.frameworks.SystemConfiguration
+      ];
+
+    env = {
+      # disable vendored packages
+      LIBGIT2_NO_VENDOR = 1;
+      LIBSSH2_SYS_USE_PKG_CONFIG = 1;
+      LUX_SKIP_IMPURE_TESTS = 1;
+    };
+  };
+
+  lux-deps = craneLib.buildDepsOnly (commonArgs
+    // {
+      src = cleanCargoSrc;
+    });
+
+  individualCrateArgs =
+    commonArgs
+    // {
+      src = cleanCargoSrc;
+      cargoArtifacts = lux-deps;
+      # NOTE: We disable tests since we run them via cargo-nextest in a separate derivation
+      doCheck = false;
+    };
+
   # can't seem to override the buildType with override or overrideAttrs :(
-  mk-lux = {buildType ? "release"}:
-    with final;
-      rustPlatform.buildRustPackage {
-        pname = "lux";
-        version = ((lib.importTOML "${self}/lux-cli/Cargo.toml").package).version;
-
-        src = self;
-
-        cargoLock = {
-          lockFile = ../Cargo.lock;
-        };
-
-        nativeBuildInputs = [
-          pkg-config
-          installShellFiles
-        ];
-
-        buildInputs =
-          [
-            luajit
-            openssl
-            libgit2
-            gnupg
-            libgpg-error
-            gpgme
-          ]
-          ++ lib.optionals stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
-
-        nativeCheckInputs = [
-          cacert
-          cargo-nextest
-          zlib # used for checking external dependencies
-          lua
-          nix # we use nix-hash in tests
-        ];
+  mk-lux-cli = {buildType ? "release"}:
+    craneLib.buildPackage (individualCrateArgs
+      // {
+        pname = "lux-cli";
+        cargoExtrArgs = "-p lux-cli";
 
         postBuild = ''
           cargo xtask dist-man
           cargo xtask dist-completions
-        '';
-
-        preCheck = ''
-          export HOME=$(realpath .)
-        '';
-
-        checkPhase = ''
-          runHook preCheck
-          # Disable integration tests
-          cargo nextest run --no-fail-fast --lib
-          runHook postCheck
         '';
 
         postInstall = ''
@@ -60,18 +68,34 @@
           installShellCompletion target/dist/lux.{bash,fish} --zsh target/dist/_lux
         '';
 
-        env = {
-          # disable vendored packages
-          LIBGIT2_NO_VENDOR = 1;
-          LIBSSH2_SYS_USE_PKG_CONFIG = 1;
-          LUX_SKIP_IMPURE_TESTS = 1;
-        };
-
         inherit buildType;
 
         meta.mainProgram = "lux";
-      };
+      });
 in {
-  lux = mk-lux {};
-  lux-debug = mk-lux {buildType = "debug";};
+  inherit lux-deps;
+  lux-cli = mk-lux-cli {};
+  lux-cli-debug = mk-lux-cli {buildType = "debug";};
+
+  lux-nextest = craneLib.cargoNextest (commonArgs
+    // {
+      src = self;
+      nativeCheckInputs = with final; [
+        cacert
+        cargo-nextest
+        zlib # used for checking external dependencies
+        lua
+        nix # we use nix-hash in tests
+      ];
+
+      preCheck = ''
+        export HOME=$(realpath .)
+      '';
+
+      cargoArtifacts = lux-deps;
+      partitions = 1;
+      partitionType = "count";
+      cargoNextestExtraArgs = "--no-fail-fast --lib"; # Disable integration tests
+      cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+    });
 }
