teto/lux

refactor!: allow building of local rockspecs

lux-lib/src/operations/resolve.rs
@@ -12,7 +12,7 @@ use crate::{
     package::PackageReq,
     progress::{MultiProgress, Progress},
     remote_package_db::RemotePackageDB,
-    rockspec::LocalRockspec,
+    rockspec::RemoteRockspec,
 };
 
 use super::{Download, RemoteRockDownload, SearchAndDownloadError};

nvim-neorocks/rocks

lux-lib/src/operations/resolve.rs
@@ -12,7 +12,7 @@ use crate::{
     package::PackageReq,
     progress::{MultiProgress, Progress},
     remote_package_db::RemotePackageDB,
-    rockspec::LocalRockspec,
+    rockspec::Rockspec,
 };
 
 use super::{Download, RemoteRockDownload, SearchAndDownloadError};